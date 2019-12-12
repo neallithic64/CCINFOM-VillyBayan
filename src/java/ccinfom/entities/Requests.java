6160b8d3228b0fef102e8af54cb374cdc159dc0b
Neal <Neal@192.168.86.125>
2019-12-12 18:57:20 +0800

    MySQL-Java connection success

--- src/java/ccinfom/entities/Requests.java
+++ src/java/ccinfom/entities/Requests.java
@@ -1,20 +1,17 @@
 package ccinfom.entities;
 
-import com.mysql.jdbc.Driver;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.sql.*;
-import java.util.Properties;
 
 public class Requests {
-	private ArrayList<BigInteger> reqList;
+	private ArrayList<Integer> reqList;
 	private ArrayList<String> reqInfo;
 	
 	public Requests() {
 		reqList = new ArrayList<>();
 		reqInfo = new ArrayList<>();
 	}
-	public ArrayList<BigInteger> getReqList() {
+	public ArrayList<Integer> getReqList() {
 		return reqList;
 	}
 	public ArrayList<String> getReqInfo() {
@@ -23,42 +20,47 @@ public class Requests {
 	
 	public boolean getAllRequests() {
 		try {
-			Driver d = new Driver();
-//			Properties p = new Properties;
-//			Connection conn = d.connect("jdbc:mysql://localhost:3307/accessservicedb", "user=admin&password=p@ssword");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/accessservicedb?useTimezone=true&serverTimezone=UTC&user=admin&password=p@ssword");
-			PreparedStatement pstmt = conn.prepareStatement("SELECT request_no FROM requests");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/accessservicedb?user=admin&password=p@ssword");
+			PreparedStatement pstmt = conn.prepareStatement("SELECT special_instruct FROM requests");
 			ResultSet rs = pstmt.executeQuery();
 			reqList.clear();
 			while (rs.next())
-				reqList.add(rs.getBigDecimal("request_no").toBigInteger());
+				reqList.add(rs.getInt("request_no"));
 			System.out.println("size of results: " + reqList.size());
 			return true;
 		} catch (Exception e) {
-			System.out.println("error! log:\n" + e.getMessage());
+			System.out.println("error! " + e.getMessage());
 			return false;
 		}
 	}
 	
 	/* supposed to store all fields of a single record into reqInfo, but still thinking
 		if it's possible to iterate through columns */
-	public boolean getReqInfo(int req_no) {
+	public boolean getReqInfo(String spec_inst) {
 		try {
 			int i = 0;
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/CCINFOM?useTimezone=true&serverTimezone=UTC&user=admin&password=p@ssword");
-			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM accessservicedb.requests WHERE request_no = ?;");
-			pstmt.setInt(1, req_no);
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/accessservicedb?user=admin&password=p@ssword");
+			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM accessservicedb.requests WHERE special_instruct = ?;");
+			pstmt.setString(1, spec_inst);
 			ResultSet rs = pstmt.executeQuery();
 			reqInfo.clear();
-//			if (rs.) {
-				reqInfo.add(rs.getString(i));
-//				i++;
-//			}
+			if (rs.next()) {
+				reqInfo.add(rs.getString(6));
+				i++;
+			}
 			System.out.println("size of AL: " + reqList.size());
 			return true;
-		} catch (SQLException e) {
-			System.out.println("error! log:\n" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("error! " + e.getMessage());
 			return false;
 		}
+	}
+	
+	public static void main(String[] args) {
+		Requests b = new Requests();
+		b.getAllRequests();
+		b.getReqInfo("None");
 	}
 }
