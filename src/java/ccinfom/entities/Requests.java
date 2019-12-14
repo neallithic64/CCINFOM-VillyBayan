e39139b0bb5d3c520a9c6c3b63e9b994ddd93cab
Neal <Neal@192.168.86.125>
2019-12-14 12:10:05 +0800

    Working createReq

--- src/java/ccinfom/entities/Requests.java
+++ src/java/ccinfom/entities/Requests.java
@@ -5,24 +5,108 @@ import java.sql.*;
 
 public class Requests {
 	private ArrayList<Integer> reqList;
-	private ArrayList<String> reqInfo;
+	private ArrayList<String> resiList;
+	
+	private int req_no;
+	private Date date_created;
+	private Date date_processed;
+	private char status;
+	private char homeservice;
+	private String special_inst;
+	private Date confirmed_date;
+	private Date cancelled_date;
+	private Date completed_date;
+	private Time confirmed_time;
+	private Time cancelled_time;
+	private double cancellation_fee;
+	private String cancellation_reason;
+	private double total_amount;
+	private String resident_email;
+	private int group_id;
+	private int slot_id;
 	
 	public Requests() {
 		reqList = new ArrayList<>();
-		reqInfo = new ArrayList<>();
+		resiList = new ArrayList<>();
 	}
 	public ArrayList<Integer> getReqList() {
 		return reqList;
 	}
-	public ArrayList<String> getReqInfo() {
-		return reqInfo;
+	public ArrayList<String> getResiList() {
+		return resiList;
+	}
+	public void setReq_no(int req_no) {
+		this.req_no = req_no;
+	}
+	public void setDate_created(Date date_created) {
+		this.date_created = date_created;
+	}
+	public void setDate_processed(Date date_processed) {
+		this.date_processed = date_processed;
+	}
+	public void setStatus(char status) {
+		this.status = status;
+	}
+	public void setHomeservice(char homeservice) {
+		this.homeservice = homeservice;
+	}
+	public void setSpecial_inst(String special_inst) {
+		this.special_inst = special_inst;
+	}
+	public void setConfirmed_date(Date confirmed_date) {
+		this.confirmed_date = confirmed_date;
+	}
+	public void setCancelled_date(Date cancelled_date) {
+		this.cancelled_date = cancelled_date;
+	}
+	public void setCompleted_date(Date completed_date) {
+		this.completed_date = completed_date;
+	}
+	public void setConfirmed_time(Time confirmed_time) {
+		this.confirmed_time = confirmed_time;
+	}
+	public void setCancelled_time(Time cancelled_time) {
+		this.cancelled_time = cancelled_time;
+	}
+	public void setCancellation_fee(double cancellation_fee) {
+		this.cancellation_fee = cancellation_fee;
+	}
+	public void setCancellation_reason(String cancellation_reason) {
+		this.cancellation_reason = cancellation_reason;
+	}
+	public void setTotal_amount(double total_amount) {
+		this.total_amount = total_amount;
+	}
+	public void setResident_email(String resident_email) {
+		this.resident_email = resident_email;
+	}
+	public void setGroup_id(int group_id) {
+		this.group_id = group_id;
+	}
+	public void setSlot_id(int slot_id) {
+		this.slot_id = slot_id;
+	}
+	
+	public void getAllResidents() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/accessservicedb?user=admin&password=p@ssword");
+			PreparedStatement pstmt = conn.prepareStatement("SELECT  FROM requests");
+			ResultSet rs = pstmt.executeQuery();
+			reqList.clear();
+			while (rs.next())
+				reqList.add(rs.getInt("request_no"));
+			System.out.println("size of results: " + reqList.size());
+		} catch (Exception e) {
+			System.out.println("error! " + e.getMessage());
+		}
 	}
 	
 	public boolean getAllRequests() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/accessservicedb?user=admin&password=p@ssword");
-			PreparedStatement pstmt = conn.prepareStatement("SELECT special_instruct FROM requests");
+			PreparedStatement pstmt = conn.prepareStatement("SELECT request_no FROM requests");
 			ResultSet rs = pstmt.executeQuery();
 			reqList.clear();
 			while (rs.next())
@@ -35,32 +119,18 @@ public class Requests {
 		}
 	}
 	
-	/* supposed to store all fields of a single record into reqInfo, but still thinking
-		if it's possible to iterate through columns */
-	public boolean getReqInfo(String spec_inst) {
+	public void newRequest() {
 		try {
-			int i = 0;
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/accessservicedb?user=admin&password=p@ssword");
-			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM accessservicedb.requests WHERE special_instruct = ?;");
-			pstmt.setString(1, spec_inst);
-			ResultSet rs = pstmt.executeQuery();
-			reqInfo.clear();
-			if (rs.next()) {
-				reqInfo.add(rs.getString(6));
-				i++;
-			}
-			System.out.println("size of AL: " + reqList.size());
-			return true;
+			PreparedStatement pstmt = conn.prepareStatement("INSERT INTO requests  VALUES (?)");
+//			pstmt.setInt(1, );
+			pstmt.execute();
+			pstmt.close();
+			conn.close();
 		} catch (Exception e) {
 			System.out.println("error! " + e.getMessage());
-			return false;
 		}
 	}
 	
-	public static void main(String[] args) {
-		Requests b = new Requests();
-		b.getAllRequests();
-		b.getReqInfo("None");
-	}
 }
--- web/createReq.jsp
+++ web/createReq.jsp
@@ -1,6 +1,27 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@page import = "java.util.*, ccinfom.entities.*" %>
 
+<!-- Columns:
+		request_no			int(8) PK
+		date_created		date
+		date_processed		date
+		status				enum('S','D','C','X','P')
+		homeservice			enum('Y','N')
+		special_instruct	varchar(100)
+		saved_date			date
+		confirmed_date		date
+		cancelled_date		date
+		completed_date		date
+		confirmed_time		time
+		cancelled_time		time
+		cancellation_fee	decimal(9,2)
+		cancellation_reason	varchar(100)
+		total_amount		decimal(9,2)
+		resident_email		varchar(100)
+		group_id			int(8)
+		slot_id				int(8)
+-->
+
 <html>
 	<head>
 		<title>Create Request</title>
@@ -16,7 +37,7 @@
 		%>
 		<h3 class="col-sm-offset-1">Create Request...</h3>
 			<div class="container">
-				<h4 class="col-sm-offset-2 container">Shortcut Requesting!</h4>
+				<!--<h4 class="col-sm-offset-2 container">Shortcut Requesting!</h4>-->
 				<form class="form-horizontal" action="procNewReq.jsp" method="POST">
 					<select class="form-control" name="itemkindselector">
 						<%	int index, size = reqReport.getReqList().size();
@@ -25,7 +46,7 @@
 						<%	}
 						%>
 					</select>
-					<br>
+					
 					<div class="form-group">
 						<label class="control-label col-sm-2" for="email">Email:</label>
 						<div class="col-sm-10">
